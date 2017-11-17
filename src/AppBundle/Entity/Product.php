dba478aee75713a89e7a6c5a1c6dd34dc4a63b8d
Ruslan Ismailov <ruslan@mavix.io>
2017-11-17 14:30:32 +0600

    Add entity

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -5,3 +5,30 @@
  * Date: 11/17/17
  * Time: 12:44 PM
  */
+
+namespace AppBundle\Entity;
+
+class Product
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+}

f40f19c8c956296a836f9698c577ffa36969fe16
Ruslan Ismailov <ruslan@mavix.io>
2018-01-14 17:59:38 +0600

    Issue #8 partial resolved

--- src/AppBundle/Controller/Categories.php
+++ src/AppBundle/Controller/Categories.php
@@ -5,6 +5,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Category;
+use AppBundle\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
 
 class Categories extends Controller
@@ -44,5 +45,20 @@ class Categories extends Controller
         $em->flush();
 
         return $this->redirectToRoute('adminMainPage');
+    }
+
+    /**
+     * @Route("/categories/{id}", name="view-category", requirements={"page": "\d+"} )
+     */
+    public function viewOneCategory($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+        $categories = $repository->find($id);
+
+        $products = $this->getDoctrine()->getRepository(Product::class);
+        $getProduct = $products->findBy(array('categoryId' => $id));
+
+        return $this->render('categories/viewCategory.html.twig', 
+            ['category' => $categories, 'products' => $getProduct]);
     }
 }

9f1f72a4b34f6de44c9ae652d026480a4338d880
Ruslan Ismailov <ruslan@mavix.io>
2018-05-14 20:52:51 +0600

    Issue #28 fixed

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,17 @@
 // highlight current day on opeining hours
-$(document).ready(function() {
+$(document).ready(function () {
     $('.opening-hours li').eq(new Date().getDay()).addClass('today');
+});
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function () {
+        $('body,html').animate({scrollTop: 0}, 800);
+    });
 });
